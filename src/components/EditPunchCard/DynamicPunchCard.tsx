a4dc5fe400d94232c69529a707064bb7f1d2a42a
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-05-15 16:26:57 -0300

    Ajustes no card de adição de ponto

--- src/components/EditPunchCard/DynamicPunchCard.tsx
+++ src/components/EditPunchCard/DynamicPunchCard.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   Card,
   CardContent,
+  Divider,
   TextField,
   ToggleButton,
   ToggleButtonGroup,
@@ -17,78 +18,107 @@ interface DynamicPunchesCardProps {
   onSave: (data: PunchEntry[]) => void;
 }
 
-export default function DynamicPunchCard({ initialData = [], onSave }: DynamicPunchesCardProps) {
-  const { punches, addPunch, removePunch, updatePunch } = useDynamicPunches(initialData);
+export default function DynamicPunchCard({
+  initialData = [],
+  onSave,
+}: DynamicPunchesCardProps) {
+  const { punches, addPunch, removePunch, updatePunch } =
+    useDynamicPunches(initialData);
 
   return (
-	<Card>
-	  <CardContent>
-		<Box sx={{ display: "flex", justifyContent: "space-between" }}>
-		  <Box>
-			<Typography variant="h5">Adicionar</Typography>
-			<Typography variant="subtitle1">04/11/2001</Typography>
-		  </Box>
-		  <Box>
-			<Button onClick={addPunch}>Adicionar batida</Button>
-		  </Box>
-		</Box>
-		<Box
-		  sx={{
-			display: "flex",
-			flexDirection: "column",
-			alignItems: "center",
-		  }}
-		>
-		  
-		  {punches.map((punch) => (
-			<Box key={punch.id} sx={{ mt: 3}}>
-				<Button onClick={() => removePunch(punch.id)}>Excluir</Button>
-			  <Box sx={{ display: "flex", gap: 2, mt: 1}}>
-				<Box
-				  sx={{
-					display: "flex",
-					flexDirection: "column",
-					gap: 2,
-					mb: 2
-				  }}
-				>
-				  <TextField
-					type="time"
-					size="small"
-					value={punch.time}
-					onChange={(e) =>
-					  updatePunch(punch.id, "time", e.target.value)
-					}
-				  />
-				  <ToggleButtonGroup
-					value={punch.type}
-					exclusive
-					onChange={(_, value) =>
-					  updatePunch(punch.id, "type", value)
-					}
-				  >
-					<ToggleButton value="Entry">Entrada</ToggleButton>
-					<ToggleButton value="Exit">Saida</ToggleButton>
-				  </ToggleButtonGroup>
-				</Box>
-				<TextField 
-					multiline 
-					rows={3} 
-					value={punch.note}
-					onChange={(e) => updatePunch(punch.id, "note", e.target.value)}	
-				/>
-			  </Box>
-			</Box>
-		  ))}
-		  {punches.length <= 0 ? (
-			<Typography variant="subtitle1">
-			  Não há pontos registrados.
-			</Typography>
-		  ) : (
-			<Button onClick={() => onSave(punches)}>Salvar alterações</Button>
-		  )}
-		</Box>
-	  </CardContent>
-	</Card>
+    <Card>
+      <CardContent>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box>
+            <Typography variant="h5">Adicionar</Typography>
+            <Typography variant="subtitle1">04/11/2001</Typography>
+          </Box>
+          <Box>
+            <Button onClick={addPunch}>Adicionar batida</Button>
+          </Box>
+        </Box>
+        <Divider sx={{ mt: 1, mb: 1 }} />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {punches.map((punch) => (
+            <Box key={punch.id} sx={{ mt: 2 }}>
+              <Box sx={{ display: "flex", justifyContent: "end", }}>
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={() => removePunch(punch.id)}
+                >
+                  Excluir
+                </Button>
+              </Box>
+              <Box sx={{ display: "flex", gap: 2, mt: 2 }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                    mb: 2,
+                  }}
+                >
+                  <TextField
+                    label="Hora"
+                    type="time"
+                    size="small"
+                    value={punch.time}
+                    onChange={(e) =>
+                      updatePunch(punch.id, "time", e.target.value)
+                    }
+                    slotProps={{
+                      inputLabel: {
+                        shrink: true,
+                      },
+                    }}
+                  />
+                  <ToggleButtonGroup
+                    value={punch.type}
+                    exclusive
+                    onChange={(_, value) =>
+                      updatePunch(punch.id, "type", value)
+                    }
+                  >
+                    <ToggleButton value="Entry">Entrada</ToggleButton>
+                    <ToggleButton value="Exit">Saida</ToggleButton>
+                  </ToggleButtonGroup>
+                </Box>
+                <TextField
+                  label="Nota"
+                  multiline
+                  rows={3}
+                  value={punch.note}
+                  onChange={(e) =>
+                    updatePunch(punch.id, "note", e.target.value)
+                  }
+                  slotProps={{
+                    inputLabel: {
+                      shrink: true,
+                    },
+                  }}
+                />
+              </Box>
+            </Box>
+          ))}
+          {punches.length <= 0 && (
+            <Typography variant="subtitle1">
+              Não há pontos registrados.
+            </Typography>
+          )}
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 1 }}>
+          <Button variant="contained" onClick={() => onSave(punches)}>
+            Salvar alterações
+          </Button>
+        </Box>
+      </CardContent>
+    </Card>
   );
 }
