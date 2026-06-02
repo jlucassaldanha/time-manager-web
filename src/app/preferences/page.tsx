1a80c95eaf4e252048651eca22840b14af7d282f
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-06-02 17:33:31 -0300

    Colocando preferencias de jornada em card

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import WorkJourneyCard from "@/components/WorkJourneyCard/WorkJourneyCard";
-import { Box } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 
 export default function WorkJourney() {
   return (
-    <Box>
-      <WorkJourneyCard />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 5,
+      }}
+    >
+      <Paper sx={{padding: 4, maxWidth: 400, display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <Box sx={{display: "flex", justifyContent: "center", mb: 5}}>
+          <Typography variant="h6">
+            Preferencia de jornada de trabalho
+          </Typography>
+        </Box>
+        <WorkJourneyCard />
+      </Paper>
     </Box>
   );
 }
